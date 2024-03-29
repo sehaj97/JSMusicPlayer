6df27f252f6d396863328ca5dc64673e852fe5eb
Sehajpreet Singh <sehajmagan@gmail.com>
2024-03-29 00:28:39 -0400

    initialized app by added style and basicc template

---
+++ script.js
@@ -0,0 +1,6 @@
+var playlistSongs = document.querySelector("#playlist-songs");
+var playButton = document.querySelector("#play");
+var pauseButton = document.querySelector("#pause");
+console.log(playlistSongs)
+console.log(playButton)
+console.log(pauseButton)
