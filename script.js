ad9df12b660e590811821851df87ac9b4bca00cd
Sehajpreet Singh <sehajmagan@gmail.com>
2024-03-31 00:10:00 -0400

    handled previous, next and display

--- script.js
+++ script.js
@@ -79,12 +79,12 @@ const allSongs = [
 ];
 
 const audio = new Audio();
-
 let userData = {
     songs: [...allSongs],
     currentSong: null,
     songCurrentTime: 0,
 };
+
 const playSong = (id) => {
     const song = userData?.songs.find((song) => song.id === id);
     audio.src = song.src;
@@ -97,39 +97,70 @@ const playSong = (id) => {
     }
     userData.currentSong = song;
     playButton.classList.add("playing");
-    pauseButton.classList.remove("playing");
 
+    highlightCurrentSong();
+    setPlayerDisplay();
+    setPlayButtonAccessibleText();
     audio.play();
 };
 
-const pauseSong = (id) => {
-    const song = userData?.songs.find((song) => song.id === id);
-    userData.currentSong = song;
+const pauseSong = () => {
     userData.songCurrentTime = audio.currentTime;
-    pauseButton.classList.add("playing");
-    playButton.classList.remove("playing");
 
+    playButton.classList.remove("playing");
     audio.pause();
 };
-playButton.addEventListener("click", () => {
+
+const playNextSong = () => {
     if (userData?.currentSong === null) {
         playSong(userData?.songs[0].id);
     } else {
-        playSong(userData?.currentSong.id);
+        const currentSongIndex = getCurrentSongIndex();
+        const nextSong = userData?.songs[currentSongIndex + 1];
+
+        playSong(nextSong.id);
     }
-});
-pauseButton.addEventListener("click", () => {
-    if (userData?.currentSong !== null) {
-        pauseSong(userData?.currentSong.id);
+};
+
+const playPreviousSong = () => {
+    if (userData?.currentSong === null) return;
+    else {
+        const currentSongIndex = getCurrentSongIndex();
+        const previousSong = userData?.songs[currentSongIndex - 1];
+
+        playSong(previousSong.id);
     }
-});
+};
+
+const setPlayerDisplay = () => {
+    const playingSong = document.getElementById("player-song-title");
+    const songArtist = document.getElementById("player-song-artist");
+    const currentTitle = userData?.currentSong?.title;
+    const currentArtist = userData?.currentSong?.artist;
+
+    playingSong.textContent = currentTitle ? currentTitle : "";
+    songArtist.textContent = currentArtist ? currentArtist : "";
+};
+
+const highlightCurrentSong = () => {
+    const playlistSongElements = document.querySelectorAll(".playlist-song");
+    const songToHighlight = document.getElementById(
+        `song-${userData?.currentSong?.id}`
+    );
+
+    playlistSongElements.forEach((songEl) => {
+        songEl.removeAttribute("aria-current");
+    });
+
+    if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
+};
 
 const renderSongs = (array) => {
     const songsHTML = array
         .map((song) => {
             return `
         <li id="song-${song.id}" class="playlist-song">
-        <button class="playlist-song-info">
+        <button class="playlist-song-info" onclick="playSong(${song.id})">
             <span class="playlist-song-title">${song.title}</span>
             <span class="playlist-song-artist">${song.artist}</span>
             <span class="playlist-song-duration">${song.duration}</span>
@@ -145,6 +176,31 @@ const renderSongs = (array) => {
 
     playlistSongs.innerHTML = songsHTML;
 };
+
+const setPlayButtonAccessibleText = () => {
+    const song = userData?.currentSong || userData?.songs[0];
+
+    playButton.setAttribute(
+        "aria-label",
+        song?.title ? `Play ${song.title}` : "Play"
+    );
+};
+
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
+
+playButton.addEventListener("click", () => {
+    if (userData?.currentSong === null) {
+        playSong(userData?.songs[0].id);
+    } else {
+        playSong(userData?.currentSong.id);
+    }
+});
+
+pauseButton.addEventListener("click", pauseSong);
+
+nextButton.addEventListener("click", playNextSong);
+
+previousButton.addEventListener("click", playPreviousSong);
 
 const sortSongs = () => {
     userData?.songs.sort((a, b) => {
